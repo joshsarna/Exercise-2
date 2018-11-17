9c6086e566ed18235c1f75d455c18c5da98843a0
joshsarna <joshsarna@gmail.com>
2018-11-16 18:14:01 -0800

    complete exercise 2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: ''
+  },
+  methods: {
+    showAlert: function() {
+      alert('Button clicked!');
+    },
+    changeValue: function(event) {
+      this.value = event.target.value;
+    }
+  }
+});
